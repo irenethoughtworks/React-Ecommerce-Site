a544a6c1204c6f012586bc4b81da8fdc32beab3d
Paula Mazo <paulamazoster@gmail.com>
2024-01-29 23:28:07 +0100

    Add unit tests for Contact component

--- Client/src/pages/contact/contact.test.js
+++ Client/src/pages/contact/contact.test.js
@@ -3,7 +3,24 @@ import "@testing-library/jest-dom";
 import { Contact } from "./contact";
 
 describe("Contact component tests suite", () => {
-  test("renders Contact component properly", () => {});
+  test("renders Contact form properly", () => { 
+    render(<Contact />);
+    const title = screen.getByText("Contact Us");
+    const labels = screen.getAllByRole("input");
 
-  test("should send contact info", () => {});
+    expect(title).toBeInTheDocument();
+    expect(labels).toHaveLength(2);
+  });
+
+  test("should send contact info", () => {
+    const handleOnSubmit = jest.fn();
+    render(<Contact onSubmit={handleOnSubmit}/>);
+    const contactButton = screen.getByText("Send");
+    
+    expect(contactButton).toBeInTheDocument();
+    
+    fireEvent.click(contactButton);
+    
+    expect(handleOnSubmit).toHaveBeenCalledTimes(1)
+  });
 })
